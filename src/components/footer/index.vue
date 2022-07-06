3377d9ffdaac3ecf9c74d81c58778030b2299a8f
sugiartofahmi <sugiartofahmi@gmail.com>
2022-07-06 20:21:08 +0700

    Fix Footer

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,31 +1,47 @@
 <template>
-
-<footer class="p-4  sm:p-6 bg-[#2D31FA]">
-    <div class="gap-8 flex justify-between px-5">
-        <div class="mb-6 md:mb-0">
-            <div  class="flex items-center">
-                <img src="../../assets/Logo-himatif.png" width="40" class="mr-4" alt="logo himatif">
-                <span class="self-center text-3xl font-freakeOne text-white ">HIMATIF</span>
-            </div>
-            <p class="mt-3 pr-10 md:w-1/2 w-full text-white">Jl. Soekarno-Hatta No.530, Sekejati, Kec. Buahbatu, Kota Bandung, Jawa Barat 40286</p>
+  <footer class="text-center lg:text-left bg-[#2D31FA] text-white">
+    <div class="mx-20 py-10 text-center md:text-left">
+      <div
+        class="grid grid-1 md:grid-cols-2 lg:grid-cols-4 gap-8 md:flex justify-between"
+      >
+        <div class="">
+          <h6
+            class="text-3xl font-freakeOne uppercase mb-4 flex items-center justify-center md:justify-start"
+          >
+            <img
+              class="mr-5"
+              src="../../assets/Logo-himatif.png"
+              width="40"
+              alt=""
+            />
+            HIMATIF
+          </h6>
+          <p class="text-lg md:w-1/2 w-full">
+            Jl. Soekarno-Hatta No.530, Sekejati, Kec. Buahbatu, Kota Bandung,
+            Jawa Barat 40286
+          </p>
         </div>
-        <div class="md:pr-20 pr-0">
-            <div>
-                <h2 class="mb-6 text-3xl font-freakeOne text-white">OUR SOCIAL MEDIA</h2>
-                <ul class="text-white ">
-                    <li class="mb-4">Email : himatifuninus@gmail.com</li>
-                    <li class="mb-4">Youtube : HIMATIF UNINUS</li>
-                    <li class="mb-4">Instagram : @himatifuninus</li>
-                </ul>
-            </div>
+
+        <div class="">
+          <h6
+            class="text-3xl font-freakeOne mb-4 flex justify-center md:justify-start"
+          >
+            OUR SOCIAL MEDIA
+          </h6>
+          <p class="mb-4 text-lg">
+            <a>Email : himatifuninus@gmail.com</a>
+          </p>
+          <p class="mb-4 text-lg">
+            <a>Youtube : HIMATIF UNINUS</a>
+          </p>
+          <p class="mb-4 text-lg">
+            <a>Instagram : @himatifuninus</a>
+          </p>
         </div>
+      </div>
     </div>
-
-    <div class="sm:flex sm:items-center sm:justify-between pl-5">
-        <span class="text-sm text-white sm:text-center ">© 2022 - 2023 HIMPUNAN MAHASISWA TEKNIK INFORMATIKA UNINUS
-        </span>
-    
+    <div class="text-lg text-center md:text-left pb-6 mx-20">
+      <span>© 2022 - 2023 HIMPUNAN MAHASISWA TEKNIK INFORMATIKA UNINUS</span>
     </div>
-</footer>
-
+  </footer>
 </template>
